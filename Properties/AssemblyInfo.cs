e3243ee566e7fc31ee3283b78f6842f94ba5eb61
Nanaimo_2013 <Jadenbowditch1@gmail.com>
2025-01-21 00:49:42 -0800

    Version 1.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4B14F1F8-BC7E-194D-2C4E-11F7292231C2")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
